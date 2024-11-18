c46c8a10bf2be560924228cf721df14dbcc05583
bishwajitsarkar99 <bishwajitsarkar99@gmail.com>
2024-11-18 18:45:44 +0600

    update

--- resources/views/sendingEmails/ajax/inbox-ajax.blade.php
+++ resources/views/sendingEmails/ajax/inbox-ajax.blade.php
@@ -47,7 +47,14 @@
                     parseInt(permission.message_status) === 1
                 ) ? '' : 'disabled';
 
-                //console.log(disableButton);
+                const disableForwardButton = Array.isArray(user_email_delete_permissions) && 
+                user_email_delete_permissions.some(permission =>
+                    parseInt(permission.report_status) === 1
+                ) ? '' : 'disabled';
+
+                // Set value dynamically based on button state
+                const value = disableButton === '' ? row.id : '';
+                const forwardValue = disableForwardButton === '' ? row.id : '';
                 
 
                 var statusClass, statusText, statusColor, statusBg;
@@ -142,10 +149,10 @@
                             <button class="btn-sm edit_registration view_btn cgr_btn viewurs ms-1" data-parent="${row.id}" id="viewBtn" email_id="${row.id}" style="font-size: 10px;" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="View" data-bs-delay="100" data-bs-html="true" data-bs-boundary="window" data-bs-template='<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner bg-flora"></div></div>'>
                                 <i class="fa-regular fa-eye fa-beat" style="margin-top: 1px;"></i>
                             </button>
-                            <button class="btn-sm edit_registration view_btn cgr_btn viewurs ms-1" data-parent="${row.id}" id="forwardBtn" value="${row.id}" style="font-size: 10px;" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Forward" data-bs-delay="100" data-bs-html="true" data-bs-boundary="window" data-bs-template='<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner bg-flora"></div></div>'>
+                            <button class="btn-sm edit_registration view_btn cgr_btn viewurs ms-1" data-parent="${row.id}" id="forwardBtn" value="${forwardValue}" style="font-size: 10px;" ${disableForwardButton} type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Forward" data-bs-delay="100" data-bs-html="true" data-bs-boundary="window" data-bs-template='<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner bg-flora"></div></div>'>
                                 <i class="fa-solid fa-share-nodes fa-beat" style="margin-top: 1px;"></i>
                             </button>
-                            <button class="btn-sm edit_registration view_btn cgr_btn ms-1" id="deleteBtn" value="${row.id}" data-email-id="${row.id}" style="font-size: 10px;" ${disableButton} type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" data-bs-delay="100" data-bs-html="true" data-bs-boundary="window" data-bs-template='<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner bg-danger"></div></div>'>
+                            <button class="btn-sm edit_registration view_btn cgr_btn ms-1" id="deleteBtn" value="${value}" data-email-id="${row.id}" style="font-size: 10px;" ${disableButton} type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" data-bs-delay="100" data-bs-html="true" data-bs-boundary="window" data-bs-template='<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner bg-danger"></div></div>'>
                                 <i class="fa-solid fa-trash-can fa-beat"></i>
                             </button>
                             <span class="child-td1 ps-1">${fromEmail ? fromEmail : row.sender_email}</span>
