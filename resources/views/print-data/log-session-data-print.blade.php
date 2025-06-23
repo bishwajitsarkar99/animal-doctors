bcd517b8d0d406a449410733bf2b8351904b0a0c
bishwajitsarkar99 <bishwajitsarkar99@gmail.com>
2025-06-23 18:46:44 +0600

    updated

--- resources/views/print-data/log-session-data-print.blade.php
+++ resources/views/print-data/log-session-data-print.blade.php
@@ -37,9 +37,14 @@
         table,tr,th,td {
             width: 100%;
             border-collapse: collapse;
+            /* common-border : if the common border is stop then will start optional border 
+                other wise will be continue common border. 
+            */
+            border: 1px solid lightgray;
         }
         th{
             background-color: rgb(239, 255, 255);
+            /* optional-border */
             border-top: 1px solid lightgray;
             border-bottom: 1px solid lightgray;
             padding: 2px;
@@ -48,12 +53,15 @@
             width: 100%;
         }
         th #theadLeftBorder{
+            /* optional-border */
             border-left: 1px solid lightgray;
         }
         th #theadRightBorder{
+            /* optional-border */
             border-right:1px solid lightgray;
         }
         tr{
+            /* optional-border */
             border: 1px solid lightgray;
             padding: 1px;
             font-size:12px;
@@ -71,12 +79,18 @@
 <body>
     <div id="session-modal-content">
         <div class="header">
-            <div class="row" style="background-color:white;border-bottom: 1px double lightgray;padding-bottom:15px;">
-                <div class="col-6">
+            <div class="row" style="background-color:white;border-bottom: 1px double lightgray;padding-bottom:20px;">
+                <div class="col-12">
                     <span style="float:inline-start;">
                         <img style="width:70px;height:55px;padding:0px;border:1px solid lightgray;" src="data:image/log/png;base64,{{ $imageData }}" alt="company-logo" width="100">
                     </span>
-                    <span style="color:black; font-size:11px; padding-right:5px;float:right;margin-top:10px;">
+                    @foreach($companyinformations as $infos)
+                        <p style="color:black; font-size:12px; text-align:left;margin-left:80px;margin-bottom: 0px;margin-top:0px;padding-top:12px;">
+                            <span style="color:black; font-size:17px; font-wight:600;">{{$infos->company_name}}</span><br>
+                            <span style="color:black; font-size:12px;">Address :{{$infos->company_address}}</span><br>
+                        </p>
+                    @endforeach
+                    <span style="color:black; font-size:11px; padding-right:5px;float:right;margin-top:-30px;">
                         Download :<?php
                         $timezone = date_default_timezone_get();
                         ?>
@@ -90,14 +104,6 @@
                         </label>
                     </span>
                 </div>
-                <div class="col-6">
-                    @foreach($companyinformations as $infos)
-                        <p style="color:black; font-size:12px; text-align:left;margin-left:80px;margin-bottom: 0px;margin-top:10px;">
-                            <span style="color:black; font-size:17px; font-wight:600;">{{$infos->company_name}}</span><br>
-                            <span style="color:black; font-size:12px;">Address :{{$infos->company_address}}</span><br>
-                        </p>
-                    @endforeach
-                </div>
             </div>
         </div>
     
@@ -109,11 +115,11 @@
                         To : {{ $end_date->format('d M Y') }}<br>
                     </p>
                 </div>
-                <div class="col-xl-6" style="float:right;">
+                <div class="col-xl-6">
                     @php
                         $firstSession = $logSessionData->first();
                     @endphp
-                    <p style="font-weight:700; font-size:11px; color:black; text-align:left;margin-bottom: 5px;padding-top: 5px;">
+                    <p style="float:right;font-weight:700; font-size:11px; color:black; text-align:left;margin-bottom: 5px;padding-top: 5px;">
                         Branch-Type : {{ optional($firstSession->users)->branch_type ?? 'N/A' }}<br>
                         Branch-ID : {{ $firstSession->branch_id ?? 'N/A' }}<br>
                         Branch-Name : {{ optional($firstSession->users)->branch_name ?? 'N/A' }}<br>
@@ -121,18 +127,18 @@
                     </p>
                 </div>
             </div>
-            <div class="row">
+            <div class="row mt-1">
                 <table style="margin-top:5px;">
                     <thead>
                         <tr>
-                            <th id="theadLeftBorder" style="text-align:center;width:10px;">SN.</th>
-                            <th style="text-align:center;width:10px;">ID</th>
-                            <th style="text-align:left;">Email</th>
-                            <th style="text-align:left;">Role</th>
-                            <th style="text-align:left;">IP</th>
-                            <th style="text-align:left;">Login Time</th>
-                            <th style="text-align:left;">Logout Time</th>
-                            <th style="text-align:left;">Last Activity</th>
+                            <th id="theadLeftBorder" style="text-align:center;width:5%;">SN.</th>
+                            <th style="text-align:center;width:5%;">ID</th>
+                            <th style="text-align:left;width:30%;">Email</th>
+                            <th style="text-align:left;width:10%;">Role</th>
+                            <th style="text-align:left;width:10%;">IP</th>
+                            <th style="text-align:left;width:15%;">Login Time</th>
+                            <th style="text-align:left;width:15%;">Logout Time</th>
+                            <th style="text-align:left;width:10%;">Last Activity</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -143,14 +149,14 @@
                         @if( count($logSessionData) >0 )
                             @foreach($logSessionData as $item)
                                 <tr>
-                                    <td style="text-align: center;">{{ $serialNumber++ }}</td>
-                                    <td style="text-align: center;">{{ $item->user_id }}</td>
-                                    <td style="text-align: left;">{{ $item->email }}</td>
-                                    <td style="text-align: left;">{{ $item->roles->name }}</td>
-                                    <td style="text-align: left;width:60px;">{{ $item->ip_address }}</td>
-                                    <td style="text-align: left;width:80px;">{{ $item->created_at->format('d M Y h:i:s A') }}</td>
-                                    <td style="text-align: left;width:80px;">{{ $item->updated_at->format('d M Y h:i:s A') }}</td>
-                                    <td style="text-align: left;width:80px;">{{ $item->last_activity }}</td>
+                                    <td style="text-align: center;width:5%;">{{ $serialNumber++ }}</td>
+                                    <td style="text-align: center;width:5%;">{{ $item->user_id }}</td>
+                                    <td style="text-align: left;width:30%;">{{ $item->email }}</td>
+                                    <td style="text-align: left;width:10%;">{{ $item->roles->name }}</td>
+                                    <td style="text-align: left;width:10%;">{{ $item->ip_address }}</td>
+                                    <td style="text-align: left;width:15%;">{{ $item->created_at->format('d M Y h:i:s A') }}</td>
+                                    <td style="text-align: left;width:15%;">{{ $item->updated_at->format('d M Y h:i:s A') }}</td>
+                                    <td style="text-align: left;width:10%;">{{ $item->last_activity }}</td>
                                 </tr>
                             @endforeach
                             @else
@@ -163,7 +169,7 @@
                     </tbody>
                 </table>
             </div>
-            <div style="width: 100%; overflow: hidden;margin-top:10px;">
+            <div style="display: flex;justify-content: space-between;width: 100%; overflow: hidden;margin-top:10px;">
                 <div style="display: inline-block; width: 40%; vertical-align: top;">
                     <table style="width: 100%; border: 1px solid lightgray;">
                         <thead>
@@ -171,7 +177,7 @@
                                 <th colspan="5" style="text-align:center;background-color:rgb(239, 255, 255);">User Summary</th>
                             </tr>
                             <tr>
-                                <th style="width: 10%;">SN.</th>
+                                <th style="width: 10%;text-align:center;">SN.</th>
                                 <th style="width: 60%;text-align:left;">Email</th>
                                 <th style="width: 10%;">Login</th>
                                 <th style="width: 10%;">Logout</th>
@@ -200,14 +206,14 @@
                     </table>
                 </div>
     
-                <div style="display: inline-block; width: 40%; vertical-align: top;margin-left:136px">
+                <div style="display: inline-block; width: 40%; vertical-align: top;">
                     <table style="width: 100%; border: 1px solid lightgray;">
                         <thead>
                             <tr style="font-weight: 700;font-size:12px;">
                                 <th colspan="5" style="text-align:center;background-color:rgb(239, 255, 255);">Branch Summary</th>
                             </tr>
                             <tr style="font-weight: 700;">
-                                <th style="width: 10%;">SN.</th>
+                                <th style="width: 10%;text-align:center;">SN.</th>
                                 <th style="width: 30%;text-align:left;">Branch-ID</th>
                                 <th style="width: 20%;">Login</th>
                                 <th style="width: 20%;">Logout</th>
@@ -230,27 +236,27 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <div style="background-color: white; color:black; text-align:left; font-size:12px;font-weight:700;">
-                    <p style="display: inline-block; margin-top:50px;">
-                        <span style="text-align: center;">
-                            <label for="prepared">
-                                Prepared by ({{Auth::User()->name}})
-                            </label>
-                        </span>
-                        <span style="text-align: center; margin-left:150px;">
-                            <label for="reference">
-                                Reference by
-                            </label>
-                        </span>
-                        <span style="text-align: center; margin-left:205px;">
-                            <label for="prepared">
-                                Authorized by
-                            </label>
-                        </span>
-                    </p>
-                </div>
+        <div class="row" style="display: flex;justify-content: space-between;color:black; text-align:left; font-size:12px;font-weight:700;">
+            <div class="col-4" style="margin-top:50px;text-align: center;">
+                <span>
+                    <label for="prepared">
+                        Prepared by ({{Auth::User()->name}})
+                    </label>
+                </span>
+            </div>
+            <div class="col-4" style="margin-top:50px;text-align: center;">
+                <span>
+                    <label for="reference">
+                        Reference by
+                    </label>
+                </span>
+            </div>
+            <div class="col-4" style="margin-top:50px;text-align: center;">
+                <span>
+                    <label for="prepared">
+                        Authorized by
+                    </label>
+                </span>
             </div>
         </div>                    
         <div class="footer" style="border-top: 1px double lightgray;">
